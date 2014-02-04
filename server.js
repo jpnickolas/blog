01964d7d6f85871bc0e336a5375f348a2570de2f
Nick Hilton <hilton@csh.rit.edu>
2014-02-04 13:00:03 -0500

    This now works as a basic web server, serving up any files in the "public" repository.

--- server.js
+++ server.js
@@ -6,7 +6,22 @@ mongoose.connect('mongodb://0.0.0.0/blog');
 
 app.configure(function() {
   app.use(express.static(__dirname+'/public/'));
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.methodOverride());
 });
+
+var Article = mongoose.model('Article', {
+  title: String,
+  content: String,
+  summary: String,
+  published: Date
+});
+
+/*
+app.get('/api/articles', function(req, res) {
+  Article.find(
+  );
+});*/
 
 app.listen(8080);
