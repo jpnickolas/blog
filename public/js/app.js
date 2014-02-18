9af6a65d03f47e27c16e86cdb54e0a956ac96d5d
Nick Hilton <hilton@csh.rit.edu>
2014-02-18 02:40:39 +0000

    Lots of updates here.
    So first of all, I changed around the fronted so that there is now
    a view page which is immutable, and the edit pages are hidden.
    
    Pages area also now tied to a plaintext url, instead of a hexadecimal
    number.
    
    Next, the server was updated to allow session for logged in users. As
    well, users cannot edit or delete posts unless logged in. The api has
    been updated so that you can check if the current user is logged in.
    
    Lastly, the api uses the url of articles for get requests, but the
    hex id for posts, because you can edit the url, and I don't want anything
    getting overwritten but mucking about with the url.

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,31 @@
 angular.module('blog', ['ngRoute','ngAnimate','ngSanitize'])
 
+//some nice factory functions 
 .factory('Articles', function($http) {
   var factory = {};
+
+  //gets the articles from the server
   factory.getArticles = function(){
     return $http.get('/api/articles');
   };
+
+  //gets a specific article from the server
   factory.getArticle = function(id) {
     return $http.get('/api/articles/'+id);
   };
+
+  //saves an article to the server
   factory.saveArticle = function(article) {
     return $http.post('/api/articles/'+article._id, {
       title:article.title,
+      url:article.url,
       content:article.content,
       summary:article.content,
       published:dateNow()
     });
   };
+
+  //deletes an article from the server
   factory.deleteArticle = function(id) {
     return $http.delete('/api/articles/'+id);
   };
@@ -23,17 +33,23 @@ angular.module('blog', ['ngRoute','ngAnimate','ngSanitize'])
   return factory;
 })
 
+
+//Controls the layout of the url in the site
 .config(function($routeProvider) {
   $routeProvider
     .when('/', {
       controller:'ListCtrl',
       templateUrl:'list.html'
     })
-    .when('/post', {
+    .when('/post/:post_id', {
+      controller: 'ViewPostCtrl',
+      templateUrl: 'view.html'
+    })
+    .when('/post/edit', {
       controller: 'NewPostCtrl',
       templateUrl: 'edit.html'
     })
-    .when('/post/:post_id', {
+    .when('/post/edit/:post_id', {
       controller:'EditPostCtrl',
       templateUrl:'edit.html'
     })
@@ -42,18 +58,22 @@ angular.module('blog', ['ngRoute','ngAnimate','ngSanitize'])
     });
 })
 
+//A nice little filter that will tell angular to trust the html instead of
+//using the html as text
 .filter('trustHTML', function($sce) {
   return function(val) {
     return $sce.trustAsHtml(val);
   };
 })
 
+//controller for listing the articles. Pretty simple
 .controller('ListCtrl', function($scope, Articles) {
   Articles.getArticles().success(function(data) {
     $scope.articles = data;
   });
 })
 
+//assigns the save function for creating new posts
 .controller('NewPostCtrl', function($scope, $routeParams, $location, Articles) {
   $scope.article={};
   
@@ -64,6 +84,7 @@ angular.module('blog', ['ngRoute','ngAnimate','ngSanitize'])
 
 })
 
+//assigns the save and edit functions for editing posts
 .controller('EditPostCtrl', function($scope, $routeParams, $location, Articles) {
  
   $scope.article = {};
@@ -80,8 +101,17 @@ angular.module('blog', ['ngRoute','ngAnimate','ngSanitize'])
     Articles.deleteArticle($scope.article._id);
     $location.path('#/');
   };
-});
+})
 
+//Very basic, just gets the article requested, and sends it to the page
+.controller('ViewPostCtrl', function($scope, $routeParams, $location, Articles) {
+  $scope.article = {};
+
+  Articles.getArticle($routeParams.post_id).success(function(data) {
+    $scope.article=data[0];
+  });
+});
+//nice helper function for getting the date
 function dateNow() {
   var today = new Date();
   var dd = today.getDate();
@@ -89,4 +119,5 @@ function dateNow() {
 
   var yyyy = today.getFullYear();
   if(dd<10){dd='0'+dd;} if(mm<10){mm='0'+mm;} today = mm+'/'+dd+'/'+yyyy;
+  return today;
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var LocalStrategy = require('passport-local').Strategy;
 
 mongoose.connect('mongodb://nickhilton.net/blog');
 
+//initialization of express app
 app.configure(function() {
   app.use(express.static(__dirname+'/public/'));
   app.use(express.logger('dev'));
@@ -20,19 +21,24 @@ app.configure(function() {
   app.use(app.router);
 });
 
+
+//creates an article type for the database
 var Article = mongoose.model('Article', {
   title: String,
+  url: String,
   content: String,
   summary: String,
   published: Date
 });
 
+//creates a user type for the database and passport to use
 var User = mongoose.model('User', {
   name: String,
   username: String,
   password: String
 });
 
+//Creates a simple username/password login system
 passport.use(new LocalStrategy( {
   usernameField: 'user',
   passwordField: 'passwd'
@@ -51,24 +57,51 @@ passport.use(new LocalStrategy( {
   })
 );
 
+/*
+ * nice helper function to tell if a user is logged in when given a user
+ *
+ * user: String - the id of the user. Should be received from req.user in
+ * the http request
+ * return: boolean - true if the user is valid, and false if not
+ */
+function loggedIn(user, callback) {
+  if(!user) 
+    callback(false)
+  
 
+  User.findOne({_id:user}, function(err, found) {
+    if(err)
+      callback(false);
+
+    if(!found)
+      callback(false);
+
+    callback(true);
+  });
+}
+
+//serializes the user to be stored in a cookie (done using mongoDB _id, so
+//I don't really have to obscure the data all that much
 passport.serializeUser( function(user, done) {
   done(null, user._id);
 });
 
+//deserializes the user by looking them up by id
 passport.deserializeUser(function(id, done) {
   User.findOne({_id:id}, function(err, user) {
     done(err, user);
   });
 });
 
+//controls login redirection
 app.post('/login',
   passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '#/login'
+    failureRedirect: '/login'
   })
 );
 
+//returns a list of the articles in json
 app.get('/api/articles', function(req, res) {
   Article.find(function(err, articles) {
     if(err) 
@@ -78,8 +111,9 @@ app.get('/api/articles', function(req, res) {
   });
 });
 
-app.get('/api/articles/:article_id', function(req, res) {
-  Article.find({_id:req.params.article_id}, function(err, article) {
+//returns a specific article based on a url stored in the database
+app.get('/api/articles/:article_url', function(req, res) {
+  Article.find({url:req.params.article_url}, function(err, article) {
     if(err)
       res.send(err);
 
@@ -87,51 +121,81 @@ app.get('/api/articles/:article_id', function(req, res) {
   });
 });
 
+//edits an article based on the id provided, or creates a new article
 app.post('/api/articles/:article_id', function(req, res) {
-  var data = req.body;
-  Article.findOne({_id:req.params.article_id}, function(err, found) {
-    if(found) {
-      found.title=data.title;
-      found.content=data.content;
-      found.summary=data.summary;
-      found.published=data.published;
-      found.save(); 
-      res.json(found);
-    }
-    else {
-      Article.create({
-        title:data.title,
-        content: data.content,
-        summary: data.summary,
-        published: data.date
-      }, function(err, article) {
-        if(err)
-          res.send(err);
-        
-        Article.find(function(err, articles) {
+  loggedIn(req.user, function(authenticated) {
+    if(!authenticated) 
+      res.json(false);
+  
+    var data = req.body;
+  
+    //checks if the article is currently in the database
+    Article.findOne({_id:req.params.article_id}, function(err, found) {
+
+      //if it is, it edits the article, rather than creating a new one
+      if(found) {
+        found.title=data.title;
+        found.url=data.url;
+        found.content=data.content;
+        found.summary=data.summary;
+        found.published=data.published;
+        found.save(); 
+        res.json(found);
+      }
+
+      //creates a new article if one is not already in the database
+      else {
+        Article.create({
+          title:data.title,
+          url:data.url,
+          content: data.content,
+          summary: data.summary,
+          published: data.date
+        }, function(err, article) {
           if(err)
             res.send(err);
-         
-          res.json(articles);
+        
+          Article.find(function(err, articles) {
+            if(err)
+              res.send(err);
+           
+            res.json(articles);
+          });
         });
-      });
-    }
+      }
+    });
   });
 });
+
+//deletes an article based its id
 app.delete('/api/articles/:article_id', function(req, res) {
-  Article.remove( {
-    _id: req.params.article_id
-  }, function(err, article) {
-    if(err)
-      res.send(err);
-    
-    Article.find(function(err, articles) {
+  loggedIn(req.user, function(authenticated) {
+    if(!authenticated) 
+      res.json(false);
+  
+    //removes the article from the database   
+    Article.remove( {
+      _id: req.params.article_id
+    }, function(err, article) {
       if(err)
         res.send(err);
-      
-      res.json(articles);
-    });
     
+      //writes out all of the articles currently in the database.
+      Article.find(function(err, articles) {
+        if(err)
+          res.send(err);
+      
+        res.json(articles);
+      });
+    
+    });
+  });
+});
+
+//checks if the user is logged in
+app.get('/api/loggedIn', function(req, res) {
+  loggedIn(req.user, function(authenticated) {
+    res.json(authenticated);
   });
 });
 
